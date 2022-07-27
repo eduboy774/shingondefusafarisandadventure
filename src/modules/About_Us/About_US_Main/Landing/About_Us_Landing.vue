22ac0bf9eede5cef8a3e0ff623861ca516a5b63d
EDWIN BEATUS <edwinbeatus69@gmail.com>
2022-07-27 07:16:06 +0300

    contact us commit

--- src/modules/About_Us/About_US_Main/Landing/About_Us_Landing.vue
+++ src/modules/About_Us/About_US_Main/Landing/About_Us_Landing.vue
@@ -1,112 +1,137 @@
 <template>
-    <v-main>
-     <br><br>
-    <v-row align="center">
-    <v-item-group
-      v-model="window"
-      class="shrink mr-6"
-      mandatory
-     color="#515934"
-    >
-      <v-item
-        v-for="n in length"
-        :key="n"
-      >
-      </v-item>
-    </v-item-group>
+  <v-main>
+    <v-container pa-10>
+      <v-layout row wrap>
+        <v-row justify="center">
+          <div class="">
+            <h1 class="orange--text font-weight-bold">About Us</h1>
+          </div>
+        </v-row>
+        <v-row align="center">
+          <v-item-group
+            v-model="window"
+            class="shrink mr-6"
+            mandatory
+            color="#515934"
+          >
+            <v-item v-for="n in length" :key="n"> </v-item>
+          </v-item-group>
 
-    <v-col lg="6">
-      <v-window
-        v-model="window"
-        class="elevation-1"
-        vertical
-        rounded
-        outline
-      >
-        <v-window-item
-          v-for="n in infos"
-          :key="n"
-        >
-          <v-card flat>
-            <v-card-text>
-              <v-row
-                class="mb-4"
-                align="center"
-              >
-                <strong class="text-h6"> {{ n.title }}</strong>
-                <v-spacer></v-spacer>
-              </v-row>
-               <h2 class="orange--text">We are Shingondefu Adventure and  Safaris</h2>
-              <p>
-                shingondefusafaris Ltd, has acquired over the last 5 years considerable experience in organizing scheduled departures for set itineraries on specific days of the week, 
-                customised individual itineraries as well as handling incentive groups for large European companies in Kenya and Tanzania. We operate from our own offices in Karatu Arusha
-                in Tanzania. Our centrally situated offices have been custom built to accommodate our Management staff, Reservations and operations departments, Accounts as well as our in-house 
-                workshops to maintain our fleet of vehicles. 
-              </p>
+          <v-col lg="6">
+            <v-window
+              v-model="window"
+              class="elevation-1"
+              vertical
+              rounded
+              outline
+            >
+              <v-window-item v-for="n in infos" :key="n">
+                <v-card flat>
+                  <v-card-text>
+                    <v-row class="mb-4" align="center">
+                      <strong class="text-h6"> {{ n.title }}</strong>
+                      <v-spacer></v-spacer>
+                    </v-row>
+                    <h2 class="orange--text">
+                      We are Shingondefu Adventure and Safaris
+                    </h2>
+                    <p>
+                      shingondefusafaris Ltd, has acquired over the last 5 years
+                      considerable experience in organizing scheduled departures
+                      for set itineraries on specific days of the week,
+                      customised individual itineraries as well as handling
+                      incentive groups for large European companies in Kenya and
+                      Tanzania. We operate from our own offices in Karatu Arusha
+                      in Tanzania. Our centrally situated offices have been
+                      custom built to accommodate our Management staff,
+                      Reservations and operations departments, Accounts as well
+                      as our in-house workshops to maintain our fleet of
+                      vehicles.
+                    </p>
 
-               <h4 class="orange--text">We provide</h4>
-               <v-list>
-            <v-list-item>
-               <v-icon color="success" class="mr-3">mdi-check-all</v-icon>
-               <v-list-item-subtitle>Modern fleet of custom built 4x4 Toyota Landcruisers to ensure the most efficient means of transport during the safari
-              </v-list-item-subtitle>
-            </v-list-item>
-             <v-list-item>
-               <v-icon color="primary" class="mr-3">mdi-check-all</v-icon>
-               <v-list-item-subtitle>Each equipped with large pop-up roof and sliding windows, ideal for photography.</v-list-item-subtitle>
-            </v-list-item>
-            <v-list-item>
-               <v-icon color="success" class="mr-3">mdi-check-all</v-icon>
-               <v-list-item-subtitle>Dedicated file handling and assistance by well trained and experienced multi-lingual staff</v-list-item-subtitle>
-            </v-list-item>
-            <v-list-item>
-               <v-icon color="primary" class="mr-3">mdi-check-all</v-icon>
-               <v-list-item-subtitle> Extensive experience in customer care and handling as well as health and safety standards and issues</v-list-item-subtitle>
-            </v-list-item>
-            <v-list-item>
-               <v-icon color="success" class="mr-3">mdi-check-all</v-icon>
-               <v-list-item-subtitle>Strong links with hotels and lodges, which were developed over the years, which ensures special rates for our clients</v-list-item-subtitle>
-            </v-list-item>
-          </v-list>
-            </v-card-text>
-          </v-card>
-        </v-window-item>
-      </v-window>
-    </v-col>
-     <v-col lg="6">
-    </v-col>
-    </v-row>
-    <br><br>
-    <Team/>
-      <Footer/>
-    </v-main>
-  
+                    <h4 class="orange--text">We provide</h4>
+                    <v-list>
+                      <v-list-item>
+                        <v-icon color="success" class="mr-3"
+                          >mdi-check-all</v-icon
+                        >
+                        <v-list-item-subtitle
+                          >Modern fleet of custom built 4x4 Toyota Landcruisers
+                          to ensure the most efficient means of transport during
+                          the safari
+                        </v-list-item-subtitle>
+                      </v-list-item>
+                      <v-list-item>
+                        <v-icon color="primary" class="mr-3"
+                          >mdi-check-all</v-icon
+                        >
+                        <v-list-item-subtitle
+                          >Each equipped with large pop-up roof and sliding
+                          windows, ideal for photography.</v-list-item-subtitle
+                        >
+                      </v-list-item>
+                      <v-list-item>
+                        <v-icon color="success" class="mr-3"
+                          >mdi-check-all</v-icon
+                        >
+                        <v-list-item-subtitle
+                          >Dedicated file handling and assistance by well
+                          trained and experienced multi-lingual
+                          staff</v-list-item-subtitle
+                        >
+                      </v-list-item>
+                      <v-list-item>
+                        <v-icon color="primary" class="mr-3"
+                          >mdi-check-all</v-icon
+                        >
+                        <v-list-item-subtitle>
+                          Extensive experience in customer care and handling as
+                          well as health and safety standards and
+                          issues</v-list-item-subtitle
+                        >
+                      </v-list-item>
+                      <v-list-item>
+                        <v-icon color="success" class="mr-3"
+                          >mdi-check-all</v-icon
+                        >
+                        <v-list-item-subtitle
+                          >Strong links with hotels and lodges, which were
+                          developed over the years, which ensures special rates
+                          for our clients</v-list-item-subtitle
+                        >
+                      </v-list-item>
+                    </v-list>
+                  </v-card-text>
+                </v-card>
+              </v-window-item>
+            </v-window>
+          </v-col>
+          <v-col lg="6"> </v-col>
+        </v-row>
+        <br /><br />
+        <Team />
+        <Footer />
+      </v-layout>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
-import Team from './Team.vue'
+import Team from "./Team.vue";
 export default {
-  components: { 
-   Team},
-    data:()=>(
-      {
-        length:1,window: 0,
-        infos:[
-        {  title:"Who are we ?"},
-        {  title:"Who are we ?"}
-
-        ] ,
-        
-
-        }
-    )
-
-}
+  components: {
+    Team,
+  },
+  data: () => ({
+    length: 1,
+    window: 0,
+    infos: [{ title: "Who are we ?" }, { title: "Who are we ?" }],
+  }),
+};
 </script>
 
 <style>
-.main
-{
-  background-image: url("../../../../assets/img/about.jpeg")
+.main {
+  background-image: url("../../../../assets/img/about.jpeg");
 }
 </style>
--- src/modules/Contact_Us/Contact_Us/Contact_Us.vue
+++ src/modules/Contact_Us/Contact_Us/Contact_Us.vue
@@ -1,18 +1,153 @@
 <template>
   <v-app>
-     <v-main>
-        <v-container>
-            contact us
-
-        </v-container>
-     </v-main>
+    <v-main>
+      <v-container pa-10>
+        <v-layout row wrap>
+          <v-row justify="center">
+            <div class="">
+              <h1 class="orange--text font-weight-bold">Contact Us</h1>
+            </div>
+          </v-row>
+        </v-layout>
+        <div class="pa-10">
+          <v-row>
+            <v-col xs12 sm="6" justify>
+              <v-layout row wrap>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4" class="d-flex">
+                    <v-text-field
+                      label="Name"
+                      outlined
+                      dense
+                      small
+                      color="orange"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4" class="d-flex">
+                    <v-text-field
+                      label="Subject"
+                      outlined
+                      dense
+                      color="orange"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-layout>
+              <v-layout row wrap>
+                <v-row>
+                  <v-col cols="11" sm="5" md="4" class="d-flex" small>
+                    <v-text-field
+                      label="email"
+                      placeholder="shingondefu@gmail.com"
+                      outlined
+                      dense
+                      color="orange"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="11" sm="5" md="4" small class="d-flex">
+                    <v-textarea
+                      label="Your Message"
+                      auto-grow
+                      outlined
+                      rows="3"
+                      row-height="25"
+                      shaped
+                      required
+                      color="orange"
+                    ></v-textarea>
+                  </v-col>
+                </v-row>
+              </v-layout>
+              <v-layout row wrap>
+                <v-row>
+                  <v-col cols="12" sm="6" class="d-flex"> </v-col>
+                  <v-col class="d-flex" cols="12" sm="6">
+                    <v-btn color="#D9946C" class="hange white--text">
+                      Submit</v-btn
+                    >
+                  </v-col>
+                </v-row>
+              </v-layout>
+            </v-col>
+            <v-col cols="12" sm="6" md="4">
+              <v-layout row wrap>
+                <h1 class="font-weight-bold" color="#525936">Let's Talk</h1>
+                <p>
+                  If you have any questions or enquiries please feel free to
+                  contact us on the details provided above or alternatively you
+                  can complete our online enquiry form and we will get back to
+                  you as soon as possible.
+                </p>
+              </v-layout>
+            </v-col>
+          </v-row>
+        </div>
+        <div class="pa-10">
+          <v-layout row wrap>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-card class="d-flex" width="200" outlined height="200">
+                  <v-list-item three-line>
+                    <v-list-item-content>
+                      <div class="text-overline mb-4">Phone</div>
+                      <v-list-item-title>
+                        +255 (0) 762 670 710  
+                      </v-list-item-title>
+                      <v-list-item-subtitle
+                        >
+                        +255 (0) 786 424 721 
+                        </v-list-item-subtitle
+                      >
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-card>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-card class="d-flex" width="200" outlined height="200">
+                  <v-list-item three-line>
+                    <v-list-item-content>
+                      <div class="text-overline mb-4">Address</div>
+                      <v-list-item-title>
+                        Tanzania Head Office
+                      </v-list-item-title>
+                      <v-list-item-subtitle
+                        >
+                        P.O.BOX 4 Karatu, Arusha 
+                        </v-list-item-subtitle
+                      >
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-card>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-card class="d-flex" width="200" outlined height="200">
+                  <v-list-item three-line>
+                    <v-list-item-content>
+                      <div class="text-overline mb-4">
+                        Working Hours</div>
+                      <v-list-item-title>
+                        07:30AM - 20:00PM  
+                      </v-list-item-title>
+                      <v-list-item-subtitle
+                        >
+                       Monday - Sunday 
+                        </v-list-item-subtitle
+                      >
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-layout>
+        </div>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  components: {
-  },
+  components: {},
 };
 </script>
 
@@ -20,5 +155,8 @@ export default {
 .hhdhd {
   width: 100;
   height: 100px;
+}
+.hange {
+  text-transform: unset !important;
 }
 </style>
