00ea1512388e46f70e227115684f4dc72aeab5ac
Edwin Pantaleo <pantaleoedwin@gmail.com>
2022-08-07 19:22:08 +0300

    trekking changes

--- src/modules/Home/Home_Main/Home_Landing/Carousel.vue
+++ src/modules/Home/Home_Main/Home_Landing/Carousel.vue
@@ -1,55 +1,42 @@
 <template>
-<v-app>
-  <v-main>
-  <v-carousel
-    cycle
-    height="950"
-    hide-delimiter-background
-    show-arrows-on-hover
-    fullscreen
-  >
-    <v-carousel-item
-      v-for="(item, i) in items"
-      :key="i"
-      :src="item.src"
-    >
-    <v-row
-          class="fill-height"
-          align="center"
-          justify="center"
-        >
-          <div class="text-h2 white--text">
-            {{ slide }} 
-          </div>
-        </v-row>
-    </v-carousel-item>
-  </v-carousel>
-  </v-main>
+  <v-app>
+    <v-main>
+      <v-carousel
+        cycle
+        height="950"
+        hide-delimiter-background
+        show-arrows-on-hover
+        fullscreen
+      >
+        <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src">
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="text-h2 white--text">
+              {{ slide }}
+            </div>
+          </v-row>
+        </v-carousel-item>
+      </v-carousel>
+    </v-main>
   </v-app>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        slides: [
-          'Discover The Unexpected Adventures'
-        ],
-        items: [
-          {
-            src: require('../../../../assets/img/masi1.jpg'),slide: 'Discover The Unexpected Adventures'
-          },
-          {
-            src: require('../../../../assets/img/ngorongoro-1920x1280.jpg')
-            
-          },
-          {
-            
-            src: require('../../../../assets/img/serengeti-500x510.jpg')
-            
-          },
-        ],
-      
-      }
-    },
-  }
+export default {
+  data() {
+    return {
+      slides: ["Discover The Unexpected Adventures"],
+      items: [
+        {
+          src: require("../../../../assets/img/masi1.jpg"),
+          slide: "Discover The Unexpected Adventures",
+        },
+        {
+          src: require("../../../../assets/img/ngorongoro-1920x1280.jpg"),
+        },
+        {
+          src: require("../../../../assets/img/serengeti-500x510.jpg"),
+        },
+      ],
+    };
+  },
+};
 </script>
--- src/modules/Trekking/Trekking_Main/TrekkingFQA.vue
+++ src/modules/Trekking/Trekking_Main/TrekkingFQA.vue
@@ -5,13 +5,10 @@
     </v-row>
     <v-row justify="center">
       <v-expansion-panels popout outlined>
-        <v-expansion-panel v-for="(item, i) in 5" :key="i">
-          <v-expansion-panel-header>Item</v-expansion-panel-header>
+        <v-expansion-panel v-for="(item, index) in FAQ" :key="index">
+          <v-expansion-panel-header>{{ item.item }}</v-expansion-panel-header>
           <v-expansion-panel-content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            {{ item.content }}
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
@@ -19,7 +16,21 @@
   </v-container>
 </template>
 <script>
-export default {};
+export default {
+  data: () => ({
+    FAQ: [
+      { item: "Where Is Mount Kilimanjaro", content: "" },
+      { item: "How Is High is Kilimanjaro", content: "" },
+      { item: "Which Kilimanjaro Route is The Best", content: "" },
+      { item: "Safety of Kilimanjaro Route", content: "" },
+      { item: "What is Easiest Route to Climb Kilimanjaro", content: "" },
+      { item: "Reason For Kilimanjaro Route Differ in Success ", content: "" },
+      { item: "What are The Best  Climb Kilimanjaro Route", content: "" },
+
+
+    ],
+  }),
+};
 </script>
 <style scoped>
 .slick-slide {
